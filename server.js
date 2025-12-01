112ed8774ad8046082adafca389afafc8ec5cf16
Stark <sahil.gen9@gmail.com>
2025-12-01 20:39:11 +0530

    first comit

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const alert = function (){
+  console.log("Hi")
+}
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 }   );
